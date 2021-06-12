0ede105a763f0307b299b1f08be479d5c19f6696
Oleks <qatests003@gmail.com>
2021-06-12 21:11:15 +0300

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    //создадим массив 10 случайных чисел и выведем в консоль среднее арифметическое
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    }
