a2c93f6bacae1c750424b572c645404aa947e443
Oleks <qatests003@gmail.com>
2021-06-12 23:35:43 +0300

    array with 10 random values and get avarage

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,16 @@
 public class Hello {
     //создадим массив 10 случайных чисел и выведем в консоль среднее арифметическое
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[] Array = new int[10];
+        for ( int i = 0; i < Array.length; i++) {
+            Array[i] = (int) (Math.random() * 100);
+            System.out.println(Array[(int) i]);
+        }
+        double sum = 0;
+
+        for (double i = 0; i < Array.length; i++) {
+            sum += Array[(int) i];
+        }
+        System.out.println("Среднее арифметическое " + sum / Array.length);
     }
-    }
+}
