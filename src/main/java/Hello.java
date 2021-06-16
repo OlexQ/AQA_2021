fe977b9f01c60cd4976b6de40e75a23413cca5b6
ext.ashcherbina <a.shcherbyna@andersenlab.com>
2021-06-16 12:29:27 +0300

    small fixes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,16 +1,17 @@
 public class Hello {
+
     //создадим массив 10 случайных чисел и выведем в консоль среднее арифметическое
     public static void main(String[] args) {
-        int[] Array = new int[10];
-        for ( int i = 0; i < Array.length; i++) {
-            Array[i] = (int) (Math.random() * 100);
-            System.out.println(Array[(int) i]);
-        }
-        double sum = 0;
+        double average = 0;
 
-        for (double i = 0; i < Array.length; i++) {
-            sum += Array[(int) i];
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+            System.out.println(array[i]);
+            average += array[i];
         }
-        System.out.println("Среднее арифметическое " + sum / Array.length);
+
+        System.out.println("Среднее арифметическое " + average / array.length);
     }
+
 }
