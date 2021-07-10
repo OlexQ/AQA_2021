df0a6f882f8e5b1b156625586c2fa24164914ec4
Oleks <qatests003@gmail.com>
2021-07-10 16:10:44 +0300

    added 1 PriceTask class and 3 tasks into SampleTasks

--- src/main/java/SampleTasks.java
+++ src/main/java/SampleTasks.java
@@ -1,14 +1,20 @@
+import com.sun.deploy.net.MessageHeader;
+
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class SampleTasks {
 
     public static void main(String[] args) {
 
-        System.out.println("Среднее арифметическое " + getAverage());
+/*        System.out.println("Среднее арифметическое " + getAverage());
         System.out.println("Факториал числа " + getFactorial(4));
-//      System.out.print("Введите код товара от 1 до 10: ");
-        System.out.println("Ввведите любое число ") ;
-        System.out.println(getEvenNumber());
+        System.out.print("Ввведите любое число ") ;
+        printEvenNumber();
+        String stringArray = arrayToString();
+        stringToArray(stringArray);
+        updateArrayWithHello();*/
+        printAction();
     }
 
     //Посчитать Факториал числа.
@@ -20,52 +26,102 @@ public class SampleTasks {
         return result;
 
     }
+
     //Посчитать среднее арифметическое массива.
-    public static double getAverage(){
-    int sum = 0;
+    public static double getAverage() {
+        int sum = 0;
 
-    int[] array = {3, 2, 5};
+        int[] array = {3, 2, 5};
         for (int i = 0; i < array.length; i++) {
-        System.out.println(array[i]);
-        sum += array[i];
-    }
-        return sum/ array.length;
+            System.out.println(array[i]);
+            sum += array[i];
+        }
+        return sum / array.length;
     }
 
-    //Вывести цену за определенный товар опираясь на его код (предусмотреть возможность введение неверного кода).
-
-/*    public static double getPrice() {
-        double price;
-        Scanner scan = new Scanner(System.in);
-        int code = scan.nextInt();
-
-    }
-        return price;
-} */
     //Вывести на экран информацию о том является ли целое число записанное в переменную n, чётным либо нечётным.
-    public static double getEvenNumber(){
+    public static void printEvenNumber() {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        if ((n%2)==0) {
-                System.out.println ("Число " + n + " четное ");
-            }
-		else
-		 if((n%1)==0) {
-         System.out.println ("Число " + n + " не четное ");
+        if (n == 0) {
+            System.out.println("Не должно быть ноль, только целые числа");
+        } else if ((n % 2) == 0) {
+            System.out.println("Число " + n + " четное ");
+        } else {
+            System.out.println("Число " + n + " не четное ");
         }
-        return n;
+    }
 
-// Перобразовать массив в строку и обртано в массив
-        public static double getConvertedString(){
+// Перобразовать массив в строку и обратно в массив
 
-
+    public static String arrayToString() {
+        int[] array = new int[]{3, 25, 5};
+        StringJoiner stringArray = new StringJoiner(",");
+        for (int item : array) {
+            stringArray.add(String.valueOf(item));
         }
-// Добавить к каждому элементу массива слова hello
+        System.out.println(stringArray);
+        return stringArray.toString();
+    }
+
+    public static void stringToArray(String stringArray) {
+        String[] array = stringArray.split(",");
+        int[] arrayInt = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            String item = array[i];
+            int number = Integer.parseInt(item);
+            arrayInt[i] = number;
+        }
+        for (int item : arrayInt) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    // Добавить к каждому элементу массива слова hello
+    public static void updateArrayWithHello() {
+        String[] array = new String[]{"Kate", "Peter", "Marry"};
+        for (int i = 0; i < array.length; i++) {
+            array[i] = "hello " + array[i];
+        }
+        for (String item : array) {
+            System.out.println(item);
+        }
+    }
+
 // Преобразовать числовой массив в boolean
+
+
+//    Создать программу которае решает проблему
+//       Если время от 8 до 12 и денег больше 10 то идем в магазин,
+//       если время больше 12 и денег больше 50 идем в кафе
+//       если денег меьше 50 и время меньше 19 то идем к соседу,
+//       если  время больше 19 и меньше 22 то смотрим телевизор,
+//       если больше 22 то идем спать
+//       Результат выводим в консоль
+
+    public static void printAction() {
+        Scanner scanHours = new Scanner(System.in);
+        System.out.println("What is the current time?");
+        int hours = scanHours.nextInt();
+
+        System.out.println("Enter summ of the money you have?");
+        Scanner scanMoney = new Scanner(System.in);
+        int money = scanMoney.nextInt();
+
+        if (hours >= 8 && hours <= 12 && money >= 10) {
+            System.out.println("go to the market");
+        } else if (hours > 12 && money >= 50) {
+            System.out.println("go to the caffe");
+        } else if (hours >= 12 && hours < 19 && money <=50) {
+            System.out.println("go to the neighbour");
+        } else if (hours >= 19 && hours < 22) {
+            System.out.println("watch TV");
+        } else if ((hours >= 22 && hours < 24) || (hours >= 0 && hours < 8)) {
+            System.out.println("go to bed");
+        } else {
+            System.out.println("I don't know what you have to do");
+        }
+
     }
-
-
-
-
-
-    }
+}
